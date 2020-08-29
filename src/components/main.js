5b3bc0445a1651dd32714a827bf48ac8e3881339
GaiSawant <grs7@rice.edu>
2020-08-29 06:51:45 +0530

    Main link change

--- src/components/main.js
+++ src/components/main.js
@@ -10,11 +10,12 @@ import Resume from './resume';
 
 const Main = () => (
   <Switch>
+  <Route path="/resume" component={Resume} />
     <Route path="/MyPortfolio" component={LandingPage} />
     <Route path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
-    <Route path="/resume" component={Resume} />
+    
   </Switch>
 )
 
