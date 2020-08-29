269b54b41f5af3fa270c5646974630017e40052d
GaiSawant <grs7@rice.edu>
2020-08-29 07:24:35 +0530

    Toggle bar

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ import Main from './components/main';
 import { Link } from 'react-router-dom';
 
 class App extends Component {
+  hideToggle() {
+    var selectorId = document.querySelector('.mdl-layout');
+    selectorId.MaterialLayout.toggleDrawer();
+}
+
   render() {
     return (
       <div className="demo-big-content">
     <Layout>
         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/MyPortfolio/">MyPortfolio</Link>}>
             <Navigation>
-                <Link to="/resume/">Resume</Link>
-                <Link to="/projects/">Projects</Link>
-                <Link to="/contact/">Contact</Link>
+                <a href="https://gaisawant.github.io/resume/">Resume</a>
+                <a href="https://gaisawant.github.io/projects/">Projects</a>
+                <a href="https://gaisawant.github.io/contact/">Contact</a>
             </Navigation>
         </Header>
         <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/MyPortfolio/">MyPortfolio</Link>}>
             <Navigation>
-              <Link to="/resume/">Resume</Link>
-              <Link to="/projects/">Projects</Link>
-              <Link to="/contact/">Contact</Link>
+              <a href="https://gaisawant.github.io/resume/">Resume</a>
+              <a href="https://gaisawant.github.io/projects/">Projects</a>
+              <a href="https://gaisawant.github.io/contact/">Contact</a>
             </Navigation>
         </Drawer>
         <Content>
